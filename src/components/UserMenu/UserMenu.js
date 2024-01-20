7ef583625e69fbbdc3cd8339bf830c7b5b763497
artanton <kabletrade@gmail.com>
2024-01-20 21:32:44 +0200

    HW8-react1

---
+++ src/components/UserMenu/UserMenu.js
@@ -0,0 +1,10 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+export const UserMenu = () => {
+  return (
+    <div>
+      <p>mango@mail.com</p>
+      <button>Logout</button>
+    </div>
+  );
+};
