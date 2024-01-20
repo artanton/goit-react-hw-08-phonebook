7b3c65f26ee8fc3dc2a09a6ef0c2e0fd86167dd3
artanton <kabletrade@gmail.com>
2024-01-20 21:46:26 +0200

    HW8-1react

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'components/Theme';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { store } from './reduxFiles/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
